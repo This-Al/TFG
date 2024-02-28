4be085c1acc650945536a7402856e6decd6a9100
Alberto <luisalberto.gon.az@gmail.com>
2024-02-28 16:27:29 +0100

    Basic room layout, working camera

--- Assets/Scripts/DungeonGeneration/Room.cs
+++ Assets/Scripts/DungeonGeneration/Room.cs
@@ -4,10 +4,40 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
+    public int Width;
+    public int Height;
+    public int X;
+    public int Y;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(RoomController.instance == null)
+        {
+            Debug.Log("You pressed play in the wrong scene");
+            return;
+        }
+
+        RoomController.instance.RegisterRoom(this);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, new Vector3(Width, Height, 0));
+    }
+
+    public Vector3 GetRoomCentre()
+    {
+        return new Vector3 ( X * Width, Y * Height);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            RoomController.instance.OnPlayerEnterRoom(this);
+        }
     }
 
     // Update is called once per frame
